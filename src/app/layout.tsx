cefed2971f85a2c1ca8e8ac6f0f721d88b16c30b
Tanay <tanayverma98@gmail.com>
2024-01-10 19:51:02 +0530

    SocketProvider built

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { twMerge } from "tailwind-merge";
 import AppStateProvider from "../lib/providers/state-provider";
 import { SupabaseUserProvider } from "../lib/providers/supabase-user-provider";
 import { Toaster } from "../components/ui/toaster";
+import { SocketProvider } from "../lib/providers/socket-provide";
 const inter = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -27,9 +28,11 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <AppStateProvider>
             <SupabaseUserProvider>
-              {children}
-              <Toaster/>
-              </SupabaseUserProvider>
+              <SocketProvider>
+                {children}
+                <Toaster />
+              </SocketProvider>
+            </SupabaseUserProvider>
           </AppStateProvider>
         </ThemeProvider>
       </body>
