039d5a2b14c618bbce5f2bc7400ba0377913108a
Tanay <tanayverma98@gmail.com>
2024-01-24 20:06:47 +0530

    Added Trash and TrashRestore

--- src/components/sidebar/native-navigation.tsx
+++ src/components/sidebar/native-navigation.tsx
@@ -5,6 +5,7 @@ import DocCollabHomeIcon from "../icons/docCollabHomeIcon";
 import DocCollabSettingsIcon from "../icons/docCollabSettingsIcon";
 import DocCollabTrashIcon from "../icons/docCollabTrashIcon";
 import Settings from "../settings/settings";
+import Trash from "../trash/trash";
 
 interface NativeNavigationProps {
   myWorkspaceId: string;
@@ -33,16 +34,12 @@ const NativeNavigation: React.FC<NativeNavigationProps> = ({
             <span>Settings</span>
           </li>
         </Settings>
-
-        <li>
-          <Link
-            className="group/native flex text-Neutrals/neutrals-7 transition-all gap-2"
-            href={`/dashboard/${myWorkspaceId}`}
-          >
+        <Trash>
+          <li className="group/native flex text-Neutrals/neutrals-7 transition-all gap-2">
             <DocCollabTrashIcon />
             <span>Trash</span>
-          </Link>
-        </li>
+          </li>
+        </Trash>
       </ul>
     </nav>
   );
